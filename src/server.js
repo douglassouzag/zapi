de8c3784fab9dc58161c78f9b46ea7a42aef8da1
Douglas <douglas.souzag@hotmail.com>
2020-11-12 17:14:25 -0300

    Fix - Caminho incorreto da pasta das sessões

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const port = args.port;
 	const browser = await puppeteer.launch({ 
 		headless: false, 
 		executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-		userDataDir: '../sessions/' + port
+		userDataDir: './sessions/' + port
 	});
 	const page = await browser.newPage();
 	await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36');	
